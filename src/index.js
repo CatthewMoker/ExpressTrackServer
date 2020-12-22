2334987a74bcd8bf0bf2f371550be32fe424c644
Matthew Coker <mattacoker@gmail.com>
2020-12-21 19:15:45 -0500

    set up mongo backend for react app

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 require('./models/User');
+require('./models/Track')
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const authRoutes = require('../src/routes/authRoutes');
 const requireAuth = require('./middlewares/requireAuth');
+const trackRoutes = require('./routes/trackRoutes');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(authRoutes);
+app.use(trackRoutes);
 
 const mongoUri = '';
 mongoose.connect(mongoUri,{
